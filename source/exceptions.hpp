15ab23d2235f74ad0f6821fa15cbbc00c8a2d1bf
nabijaczleweli <nabijaczleweli@gmail.com>
2016-02-16 17:09:59 +0100

    This is C++, goddammit

--- source/exceptions.hpp
+++ source/exceptions.hpp
@@ -19,51 +19,27 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+
 #pragma once
 
-#ifndef EXCEPTIONS_HPP
-#define EXCEPTIONS_HPP
 
 #include <stdexcept>
 #include <string>
 
+
 class simplesmart_exception : public std::exception {
-	private:
-		unsigned int msglen, messagelen;
-		char * message;
-	public:
-		simplesmart_exception(const std::string & msg) : simplesmart_exception(msg.c_str()) {}
-		simplesmart_exception(const char * const msg) : msglen(strlen(msg)), messagelen(msglen + 89), message(new char[messagelen + 1]) {
-			memcpy(message, "Error in apoSimpleSmart: ", 25);
-			memcpy(message + 25, msg, msglen);
-			memcpy(message + 25 + msglen, ".\n\t\tPlease contact nabijaczleweli@gmail.com for further support.", 65);
-		}
-		simplesmart_exception(const simplesmart_exception & exc) : msglen(exc.msglen), messagelen(exc.messagelen), message(new char[messagelen + 1]) {
-			memcpy(message, exc.message, messagelen + 1);
-		}
-		simplesmart_exception(simplesmart_exception && exc) : msglen(exc.msglen), messagelen(exc.messagelen), message(exc.message) {
-			exc.msglen = -1;
-			exc.messagelen = -1;
-			exc.message = 0;
-		}
-		const char * what() const noexcept(true) {
-			return message;
-		}
-		~simplesmart_exception() {
-			delete[] message;
-			message = nullptr;
-		}
-		simplesmart_exception & operator=(const simplesmart_exception & other) {
-			delete[] message;
+private:
+	std::string message;
 
-			msglen = other.msglen;
-			messagelen = other.messagelen;
-			message = new char[messagelen + 1];
-			memcpy(message, other.message, messagelen + 1);
+public:
+	simplesmart_exception(const char * const msg)
+	      : message("Error in apoSimpleSmart: " + msg + ".\n\t\tPlease contact nabijaczleweli@gmail.com for further support.") {}
 
-			return *this;
-		}
+	simplesmart_exception(const std::string & msg) : simplesmart_exception(msg.c_str()) {}
+	simplesmart_exception(const simplesmart_exception & exc) : message(exc.message) {}
+	simplesmart_exception(simplesmart_exception && exc) : message(std::move(exc.message)) {}
+
+	const char * what() const noexcept {
+		return message.c_str();
+	}
 };
-
-#endif  // EXCEPTIONS_HPP
-
